b241510c8caaf162bd5a6e3c4955330af983831f
dangdat01 <dangdatcd01@gmail.com>
2021-11-02 11:40:09 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8-registration-login-example-3g5gxd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8-registration-login-example-3g5gxd)
---
+++ app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from './_services';
+import { User } from './_models';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
+export class AppComponent {
+    currentUser: User;
+    chart: {
+        value1: 10,
+        value2: 20,
+        value3: 30,
+        value4: 40,
+        value5: 50,
+        value6: 60,
+        value7: 70
+    }
+
+
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) {
+        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+    }
+
+    logout() {
+        this.authenticationService.logout();
+        this.router.navigate(['/login']);
+    }
+}
